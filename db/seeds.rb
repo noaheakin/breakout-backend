df1179475b39ef682b9f4316d397884e9a40ddfa
noaheakin <noaheakin@gmail.com>
2020-10-20 09:54:34 -0700

    added platform, ball, and blocks seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,17 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 grid1 = Grid.create(label: "Level 1")
+
+platform1 = Platform.create(x: 200, y: 400, width: 40, height: 10, speed: 2, grid_id: 1)
+
+ball1 = Ball.create(x: 200, y:350, width: 5, height: 5, speed: 1, direction: 1, grid_id: 1)
+
+block1 = Block.create(x: 10, y: 10, width: 30, height: 10, grid_id: 1)
+block2 = Block.create(x: 50, y: 10, width: 30, height: 10, grid_id: 1)
+block3 = Block.create(x: 90, y: 10, width: 30, height: 10, grid_id: 1)
+block4 = Block.create(x: 130, y: 10, width: 30, height: 10, grid_id: 1)
+block5 = Block.create(x: 170, y: 10, width: 30, height: 10, grid_id: 1)
+block5 = Block.create(x: 210, y: 10, width: 30, height: 10, grid_id: 1)
+block5 = Block.create(x: 250, y: 10, width: 30, height: 10, grid_id: 1)
+block5 = Block.create(x: 290, y: 10, width: 30, height: 10, grid_id: 1)
+block5 = Block.create(x: 330, y: 10, width: 30, height: 10, grid_id: 1)
