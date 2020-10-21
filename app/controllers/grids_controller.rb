184610e4d680fc45309136496ae42177ca574d69
noaheakin <noaheakin@gmail.com>
2020-10-21 09:40:41 -0700

    added index method to grids controller

--- app/controllers/grids_controller.rb
+++ app/controllers/grids_controller.rb
@@ -1,5 +1,10 @@
 class GridsController < ApplicationController
     
+    def index 
+        grids = Grid.all 
+        render json: grids
+    end
+    
     def show 
         grid = Grid.find(params[:id])
         render json: grid
