83b7800a912d80074b7712b5a9773bf853fd0cbc
noaheakin <noaheakin@gmail.com>
2020-10-22 10:19:12 -0700

    added routes for users and scores

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
   resources :blocks
   resources :balls
   resources :grids
+  resources :users
+  resources :scores
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
