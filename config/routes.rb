768a95bba85e3eb16960a1bde30997b0373daf27
Miqotes <ashleelennab@gmail.com>
2020-10-22 10:41:52 -0700

    fixing merge conflicts with routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  resources :platforms
-  resources :blocks
-  resources :balls
-  resources :grids
+  # resources :platforms
+  # resources :blocks
+  # resources :balls
+  # resources :grids
   resources :users
   resources :scores
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
